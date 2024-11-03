edc8dee4e4613e9160b4e21d5f2f1deff938d25d
Anna Tuhusova <bilachayka@mail.com>
2024-11-03 11:45:29 +0100

    make build

---
+++ index.75f8eb74.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.left="".concat((c-i)/2,"px"),t.style.top="".concat((l-n)/2,"px");
+//# sourceMappingURL=index.75f8eb74.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const centerX = (wallWidth - spiderWidth) / 2;
+const centerY = (wallHeight - spiderHeight) / 2;
+spider.style.left = `${centerX}px`;
+spider.style.top = `${centerY}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
